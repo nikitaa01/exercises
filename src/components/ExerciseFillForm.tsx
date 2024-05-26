75512bb70d240e558f04318ebd5fc4945529b4b5
Nikita Kuzmenko <nikitakuzmenko1107@gmail.com>
2024-05-26 23:38:47 +0200

    fix types

--- src/components/ExerciseFillForm.tsx
+++ src/components/ExerciseFillForm.tsx
@@ -1,6 +1,6 @@
 import type { Tables } from "../types/database";
 
-export default function ExerciseFillForm({ exercise, lastRepetitions, averageRepetitions }: { exercise: Tables<"exercises">, lastRepetitions: any, averageRepetitions: any }) {
+export default function ExerciseFillForm({ exercise }: { exercise: Tables<"exercises"> }) {
   return (
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {exercise.name}
