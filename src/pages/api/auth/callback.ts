155f7f2d725e95b4693faaa5d6737b63bc1fc040
Nikita Kuzmenko <nikitakuzmenko1107@gmail.com>
2024-05-27 20:11:59 +0200

    store cookie

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -9,11 +9,20 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
     return new Response("No se proporcionó ningún código", { status: 400 });
   }
 
-  const { error } = await supabase.auth.exchangeCodeForSession(authCode);
+  const { data, error } = await supabase.auth.exchangeCodeForSession(authCode);
 
   if (error) {
     return new Response(error.message, { status: 500 });
   }
+
+  const { access_token, refresh_token } = data.session;
+
+  cookies.set("sb-access-token", access_token, {
+    path: "/",
+  });
+  cookies.set("sb-refresh-token", refresh_token, {
+    path: "/",
+  });
 
   return redirect("/");
 };
